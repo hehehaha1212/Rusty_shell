5482045d32c3d2e304254f679a7e90a61063cea9
hehehaha1212 <kshatri63@gmail.com>
2026-06-09 15:01:05 +0530

    initial command read

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,10 @@ use std::io::{self, Write};
 fn main() {
     
      print!("$ ");
-     io::stdout().flush().unwrap();
+     io::stdout().flush().unwrap(); //unwrap returns the value if present and error if type or presense is not correct
+     let mut command = String:: new();   //makes us a new mutable string variable
+     io::stdin().read_line(&mut command).unwrap();
+     println!("{}: command not found", command.trim());
+    
+   
 }
