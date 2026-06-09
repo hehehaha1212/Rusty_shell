edef6949a1199e57711a9f16430c35ea19d5fa0d
hehehaha1212 <kshatri63@gmail.com>
2026-06-09 19:16:52 +0530

    program termination

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ fn main() {
      io::stdout().flush().unwrap(); //unwrap returns the value if present and error if type or presense is not correct
      let mut command = String:: new();   //makes us a new mutable string variable
      io::stdin().read_line(&mut command).unwrap();
+     command=command.trim().to_string();
+
+     if (command.to_lowercase() == "exit") || (command.to_lowercase()== "quit") {
+        break;
+     }
+
      println!("{}: command not found", command.trim());
     
     }
