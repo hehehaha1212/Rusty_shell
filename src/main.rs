748fbc40223d04c546447d14232b88216bccfc07
hehehaha1212 <kshatri63@gmail.com>
2026-06-09 19:03:13 +0530

    basic REPL (Read-Eval-Print Loop)

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use std::io::{self, Write};
 
 fn main() {
     
+    loop{
      print!("$ ");
      io::stdout().flush().unwrap(); //unwrap returns the value if present and error if type or presense is not correct
      let mut command = String:: new();   //makes us a new mutable string variable
      io::stdin().read_line(&mut command).unwrap();
      println!("{}: command not found", command.trim());
     
+    }
    
 }
